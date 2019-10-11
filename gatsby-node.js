958bd01a25a2c611a6f6147d59b52757db4ec2bf
David Wesst <david.wesst@umanitoba.ca>
2019-10-11 14:04:19 -0500

    generating slugs for dw blog posts

---
+++ gatsby-node.js
@@ -0,0 +1,7 @@
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+exports.onCreateNode = ({ node, getNode }) => {
+    if(node.internal.type === `MarkdownRemark`) {
+        console.log(createFilePath({ node, getNode, basePath: `pages`}))
+    }
+}
