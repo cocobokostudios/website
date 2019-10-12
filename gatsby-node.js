086bac3653d064b3ce6d757e8f004af0e8f9934d
David Wesst <david@cocobokostudios.com>
2019-10-11 20:56:57 -0500

    adding slug field to nodes

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,13 @@
-const { createFilePath } = require(`gatsby-source-filesystem`)
+const { createFilePath } = require(`gatsby-source-filesystem`);
 
-exports.onCreateNode = ({ node, getNode }) => {
+exports.onCreateNode = ({ node, getNode, actions }) => {
+    const { createNodeField } = actions;
     if(node.internal.type === `MarkdownRemark`) {
-        console.log(createFilePath({ node, getNode, basePath: `pages`}))
+        const slug = createFilePath({ node, getNode, basePath: `pages`});
+        createNodeField({
+            node,
+            name: `slug`,
+            value: slug
+        });
     }
 }
