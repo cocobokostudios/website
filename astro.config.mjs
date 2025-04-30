1f8428401c4f48f67115db345e9b5cee7352b6db
David Wesst <david@cocobokostudios.com>
2025-04-30 19:01:44 +0000

    fix. Fixed the bad reference left in the config page and restored it.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+
 import react from '@astrojs/react';
+
+import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://cocoboko.github.io',
   base: '/website',
-  integrations: [tailwind(), react()],
+  
+  integrations: [react(), sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
