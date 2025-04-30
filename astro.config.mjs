faa965d3ae6714de4fa8bc9b4532e12a8a574d62
David Wesst <david@cocobokostudios.com>
2025-04-30 18:59:08 +0000

    fix. Updated project to use /website path for hosting during initial development.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-
-import sitemap from '@astrojs/sitemap';
-import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), sitemap()],
-
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  site: 'https://cocoboko.github.io',
+  base: '/website',
+  integrations: [tailwind(), react()],
 });
